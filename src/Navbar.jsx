9740e1ab1f2793e71ec8ea8ead1ba38ee3225032
Auxence MEDJA <auxencemedja@gmail.com>
2024-04-24 11:23:59 -0400

    fixed logo and header title

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg primary-color p-0">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/acceuil">
-                            <img src="/src/assets/images/logo.png" alt="logo" width="55" height="55"/>
+                            <img src="/src/assets/images/logo.png" alt="logo" width="50" height="50"/>
                         </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
