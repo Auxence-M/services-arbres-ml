c3c2d8f27c9d1c3267efd125dcc511494e246e52
Auxence MEDJA <auxencemedja@gmail.com>
2024-04-25 21:35:17 -0400

    updated base of felling page

--- src/pages/Felling.jsx
+++ src/pages/Felling.jsx
@@ -1,8 +1,17 @@
+import PageImage from "../PageImage";
+
 function Felling() {
+
+    const fellingImageStyle = {
+        backgroundImage: `url("/src/assets/images/felling-img.jpg")`
+    }
     return(
-        <div className="container-fluid">
-            <h1>Page abattage</h1>
-        </div>
+        <div className="felling-service">
+            <PageImage style={fellingImageStyle} title={"Abattage"}></PageImage>
+
+            <div className="container-fluid mt-3">
+            </div>
+        </div>        
     );
 }
 
