d0daa3dcbb1ffb5f8d2cf1ce82c864992b3ae084
Auxence MEDJA <auxencemedja@gmail.com>
2024-04-25 21:39:00 -0400

    updated base of shredding page

--- src/pages/Shredding.jsx
+++ src/pages/Shredding.jsx
@@ -1,7 +1,17 @@
+import PageImage from "../PageImage";
+
 function Shredding() {
+
+    const shreddingImageStyle = {
+        backgroundImage: `url("/src/assets/images/shredding-img.jpg")`
+    }
+
     return(
-        <div className="shredding">
-            <h1>Page Homme au sol</h1>
+        <div className="shredding-service">
+            <PageImage style={shreddingImageStyle} title={"Homme au sol"}></PageImage>
+
+            <div className="container-fluid mt-3">
+            </div>
         </div>
     );
 }
