63b2aa09d6feb6e383277b3550d5a15d8c9cf5dd
Auxence MEDJA <auxencemedja@gmail.com>
2024-05-01 21:01:19 -0400

    Changed footer logo size

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
                         <div className="row">
                             <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                                 {/* <h6 className="text-uppercase mb-3 fw-bold">ArboML</h6> */}
-                                <img src="/src/assets/images/logo-footer.png" alt="logo" width="95" height="95"/>
+                                <img src="/src/assets/images/logo-footer.png" alt="logo" width="100" height="100"/>
                                 <p className="my-0 mb-2">
                                     Services d'entretien d'arbres dans la rive-sud de Montréal.
                                 </p>
