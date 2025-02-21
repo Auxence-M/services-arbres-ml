e35caca7bf4a8916f31c65ccd09d704d9eb765c5
Auxence MEDJA <auxencemedja@gmail.com>
2025-02-21 02:01:58 +0100

    Camel case fixing

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -54,7 +54,7 @@ function Footer() {
                                     onClick={toggleServiceCollapse}> 
                                     Services 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up footer-collapse-chevron" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
+                                        <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
                                     </svg>
                                 </p>
 
@@ -83,7 +83,7 @@ function Footer() {
                                     onClick={toggleContactCollapse}>
                                     Contact
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up footer-collapse-chevron" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
+                                        <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
                                     </svg>
                                 </p>
 
